ae6cbc98c605803464729eaabcb4c65c9eacb6fe
CaioTBarbieri <caiotbbarbieri@gmail,com>
2025-06-03 13:36:53 -0300

    b

---
+++ FATEC/Projeto Integrador (P.I)/LoginUBS/LoginUBS/src/main/java/com/loginUbs/LoginUBS/LoginUbsApplication.java
@@ -0,0 +1,13 @@
+package com.LoginUBS.LoginUBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginUbsApplication.class, args);
+	}
+
+}
